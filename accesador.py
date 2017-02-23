fd9620ce653a53ebcdbecb1140f162f5c450c722
Victor Albarran <victor.a.albarran.n@gmail.com>
2017-02-23 11:50:36 -0300

    Mejoramiento de la usabilidad de los scripts

--- accesador.py
+++ accesador.py
@@ -1,10 +1,9 @@
 import json
-import lib.scraper2 as scraper
+import lib.scraper as scraper
 import sys
 import os
 
 #Descarga una imagen en base a una consulta y la guarda en una ruta especificada
-#AL ejecutar requiere dos argumentos, el primero es la consulta de imagenes a descargar y la segunda la ruta donde se guardara la imagen a descargar.
 def generarDirectorio(query,ruta):
 	anteDirectorio = ruta
 	directorio = anteDirectorio + query
@@ -12,16 +11,12 @@ def generarDirectorio(query,ruta):
 		os.makedirs(directorio)
 	return directorio
 
-def main():
-    for arg in sys.argv[1:]:
-        print arg
-    query = sys.argv[1]
-    ruta = sys.argv[2]
-    cantidad = sys.argv[3]
-    rutaDirectorio = generarDirectorio(query, ruta)
-    cantidad = int(cantidad)
-    print scraper.scrapear(query, rutaDirectorio, cantidad)
-
-if __name__ == "__main__":
-    main()
-
+query = raw_input("Ingrese su busqueda: ")
+ruta = raw_input("Ingrese ruta donde descargar. (Ejemplo, /home/valbarran):")
+ruta = ruta + '/'
+cantidad = raw_input("Ingrese cantidad de imagenes a descargar: ")
+rutaDirectorio = generarDirectorio(query, ruta)
+cantidad = int(cantidad)
+orden = 0 #El orden se refiere a cual resultado, de una lista de resultados del 0 al 99 desea descargar, por defecto descarga el primer resultado, osea el numero 0
+for i in range(cantidad):
+    scraper.scrapear(query, (i + 1), rutaDirectorio, i)
--- filtro.py
+++ filtro.py
@@ -3,17 +3,16 @@ import json
 import commands
 import sys
 import lib.scraper as scraper
+import os
+import shutil
 
-def main():
-    ruta = sys.argv[1]#Ruta donde estan las imagenes
-    trozoRuta = ruta.split("/")
-    imagen = sys.argv[2]#imagen especifica a reemplazar
-    orden = sys.argv[3]#cual resultado reemplazará al resultado anterior
-    orden = int(orden)
-    query = trozoRuta[len(trozoRuta) - 1]
-    guardarComo = imagen
-    print query
-    #scraper.scrapear(query,guardarComo,ruta,orden)
-
-if __name__ == "__main__":
-    main()
+ruta = raw_input("Ingrese ruta donde estan las imagenes donde de estas usted quiere reemplazar una. (Ejemplo: /home/valbarran): ")#Ruta donde estan las imagenes
+trozoRuta = ruta.split("/")
+imagen = raw_input("Ingrese nombre de imagen a reemplazar(incluyendo extension): ")#imagen especifica a reemplazar
+orden = raw_input("Ingrese en un orden del 0 al 99, cual es el numero del resultado que quiere descargar: ")#cual resultado reemplazará al resultado anterior
+orden = int(orden)
+query = trozoRuta[len(trozoRuta) - 1]
+guardarComo = imagen.split(".")[0]
+print guardarComo
+os.remove(ruta + '/' + imagen)
+scraper.scrapear(query,guardarComo,ruta,orden)
